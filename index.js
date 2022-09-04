dad8820ad6ae310974016304b98d08ec1c78b1d4
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-09-04 14:12:40 -0400

    commit tree 299d3281bf8a98a70e2849e50f0fec4378c8d6f0

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => 
+  tutorials.map(obj => obj.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' '))
+
+// const titleCased = () => tutorials.map(updateCase);
+
+// function updateCase(obj){
+// obj = obj.split(' ');
+//   for(i = 0; i < obj.length; i++){
+//     obj[i] = obj[i][0].toUpperCase() + obj[i].slice(1);  
+//   }
+//  return obj.join(' ');
+// }
+
+
+  
+  
